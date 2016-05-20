183ad1fa831cb64744a8fa9d3f63e97c590a61e0
Tyler Smith <tyler@plantarum.ca>
2016-05-20 15:09:35 -0400

    Updated models.R
    Ditched the old code, which included lots of hand-coded functions and
    the self-starting stuff that ultimately was too awkward to use. Now
    using generating and storing initial values separate from the actual NLS
    functions. This gets around some issues with hard-coded environment
    stuff in the selfstarting code.

--- R/models.R
+++ R/models.R
@@ -1,28 +1,67 @@
 ## Contains the various model components used in the flowPloidy analysis.
 
-#####################################################################
-## singleCutBase is the un-vectorized version - not for users, who ##
-## should use singleCut instead!                                   ##
-#####################################################################
-singleCutBase <- function(SCa, intensity, xx){
-  ## Modfit appears to cut off the debris slightly above the lowest data,
-  ## which gives a better fit. The index here is the offset from the first
-  ## channel with data in it to start fitting the singleCut model.
-  ## Hard-coded to 2 it will always start at the *second* channel with data
-  ## in it. Need to test this and determine best fit. Could be a better
-  ## hard-coded value, or use an extra model parameter to dynamically fit
-  ## each time.
-  first.channel <- which(intensity > 0)[2]
+## Functions for building non-linear models for application to flowHist
+## objects. 
 
-  res <- 0
-  if(xx >= first.channel & xx < length(intensity)){
-    channels = (xx + 1):length(intensity)
-    for(j in channels){
-      res <- res + j^(1/3) * intensity[j] * 2 / (pi * j * sqrt(xx/j * (1 - xx/j)))
-    }
-  }
-  return(SCa * res)
+##' @title Gaussian model components
+##' 
+##' Components for modeling Gaussian features in flow histograms
+##'
+##' Typically the complete models will contain fA1 and fB2, which model the
+##' G1 peaks of the sample and the standard. In most cases, they will also
+##' contain fA2 and fB2, which model the G2 peaks. The G2 peaks are linked
+##' to the G1 peaks, in that they require some of the parameters from the
+##' G1 peaks as well (mean and standard deviation).
+##' 
+##' @param a1,a2,b1,b2 area parameters
+##' @param Ma,Mb curve mean parameter
+##' @param Sa,Sb curve standard deviation parameter
+##' @param xx vector of histogram intensities
+##' @return
+##' @author Tyler Smith
+##' @name gauss
+fA1 <- function(a1, Ma, Sa, xx){
+  (a1 / (sqrt(2 * pi) * Sa) * exp(-((xx - Ma)^2)/(2 * Sa^2)))
 }
+attr(fA1, "compName") <- "fA1"
+
+#' @rdname gauss
+fA2 <- function(a2, Sa, Ma, xx){
+  (a2 / (sqrt(2 * pi) * Sa * 2) * exp(-((xx - Ma * 2)^2)/(2 * (Sa * 2)^2)))
+}
+attr(fA2, "compName") <- "fA2"
+
+#' @rdname gauss
+fB1 <- function(b1, Mb, Sb, xx){
+  (b1 / (sqrt(2 * pi) * Sb) * exp(-((xx - Mb)^2)/(2 * Sb^2)))
+}
+attr(fB1, "compName") <- "fB1"
+
+#' @rdname gauss
+fB2 <- function(b2, Sb, Mb, xx){
+  (b2 / (sqrt(2 * pi) * Sb * 2) * exp(-((xx - Mb * 2)^2)/(2 * (Sb * 2)^2)))
+}
+attr(fB2, "compName") <- "fB2"
+
+## Single-cut debris model
+## 
+## S(x) = a ⱼ₌ₓ₊₁∑ⁿ ³√j YⱼPₛ(j, x)
+## Pₛ(j, x) = 2 / (πj √(x/j)(1 - x/j)
+##
+## a = amplitude parameter
+## Yⱼ = intensity in channel j
+## Pₛ(j, x) = probability of a nuclei from channel j falling into channel x
+## when cut.
+##
+## By this formula, the debris intensity in a channel/bin is a function of
+## the intensity in all the subsequent bins. This recursive relationship is
+## tricky to code; in order to take full advantage of all of the R tools
+## that support nls, the mean function needs to return one fitted value for
+## one predictor value. The following implementation of singleCut therefore
+## takes the entire vector of the response vector (intensity), necessary to
+## calculate the debris curve, and returns only the value for a single
+## predictor value.
+
 
 #' Single-cut debris model
 #'
@@ -37,21 +76,17 @@ singleCutBase <- function(SCa, intensity, xx){
 #' Yⱼ is the histogram intensity for channel j.
 #' 
 #' where Pₛ(j, x) is the probability of a nuclei from channel j falling
-#'   into channel x when cut. That is, for j > x, the probability that
-#'   fragmenting a nuclei from channel j with a single cut will produce a
-#'   fragment of size x. This probability is calculated as:
+#' into channel x when cut. That is, for j > x, the probability that
+#' fragmenting a nuclei from channel j with a single cut will produce a
+#' fragment of size x. This probability is calculated as:
 #'
 #' Pₛ(j, x) = 2 / (πj √(x/j)(1 - x/j)
 #'
 #' This model involves a recursive calculation, since the fitted value for
-#'   channel x depends not just on the intensity for channel x, but also
-#'   the intensities at all channels > x. Consequently, this is coded with
-#'   an internal loop, and then vectorized to produce a well-behaved
-#'   function that we can use with the standard nls toolchain.
-#'
-#' Only the final vectorized function \code{singleCut} is exported for the
-#'   use of users. The underlying code is in the internal function
-#'   \code{singleCutBase}
+#' channel x depends not just on the intensity for channel x, but also the
+#' intensities at all channels > x. Consequently, this is coded with an
+#' internal loop, and then vectorized to produce a well-behaved function
+#' that we can use with the standard nls toolchain.
 #'
 #' @name singleCut
 #' 
@@ -69,104 +104,145 @@ singleCutBase <- function(SCa, intensity, xx){
 #' @author Tyler Smith
 
 #' @rdname singleCut
-#' @export
-singleCut <- Vectorize(singleCutBase, "xx")
+singleCutBase <- function(SCa, intensity, xx){
+  ## Do not extend the model below/beyond the data
+  ## Modfit appears to cut off the debris slightly above the lowest data,
+  ## which gives a better fit. Perhaps set first.channel to 2-4? Need to
+  ## test this and determine best fit. Possibly use an extra parameter to
+  ## tune this for each data set individually.
+  first.channel <- which(intensity > 0)[2]
 
-#########################
-## Gaussian components ##
-#########################
-
-#' Gaussian model components
-#'
-#' Utility functions for modeling Gaussian/normal components in FCS
-#' histograms. Three 'versions' are provided, A, B, and C - they are
-#' identical except for the prefix letters.
-#'
-#' Each component contains a two peaks, representing the G1 and G2
-#' peaks. The G2 peak is constrained to have a mean and standard deviation 
-#' twice that of the G1 peak values.
-#'
-#' @name gausModel
-#'
-#' @param a1,a2,b1,b2,c1,c2 numeric values, peak area
-#' @param Ma,Mb,Mc numeric values, peak mean
-#' @param Sa,Sb,Sc numeric values, peak standard deviation
-#' @param xx numeric vector, intensity values for each channel
-NULL
-
-#' @rdname gausModel
-#' @export
-gaussA  <- function(a1, Ma, Sa, a2, xx){
-    a1 / (sqrt(2 * pi) * Sa) * exp(-((xx - Ma)^2)/(2 * Sa^2)) +
-      a2 / (sqrt(2 * pi) * Sa * 2) * exp(-((xx - Ma * 2)^2)/(2 * (Sa * 2)^2))
+  res <- 0
+  if(xx >= first.channel & xx < length(intensity)){
+    channels = (xx + 1):length(intensity)
+    for(j in channels){
+      res <- res + j^(1/3) * intensity[j] * 2 / (pi * j * sqrt(xx/j * (1 - xx/j)))
+    }
+  }
+  return(SCa * res)
 }
 
-#' @rdname gausModel
-#' @export
-gaussB  <- function(b1, Mb, Sb, b2, xx){
-  b1 / (sqrt(2 * pi) * Sb) * exp(-((xx - Mb)^2)/(2 * Sb^2)) +
-    b2 / (sqrt(2 * pi) * Sb * 2) * exp(-((xx - Mb * 2)^2)/(2 * (Sb * 2)^2))
+## Need to Vectorize this so our comparisons in line 2 make sense. Without
+## this, the comparisons try to test all values of the xx vector against
+## first.channel and length(intensity). This raises a warning, and the
+## results are not what we want. We need them tested one at a time, hence
+## the vectorize here: 
+singleCutVect <- Vectorize(singleCutBase, "xx")
+
+#' @rdname singleCut
+singleCut <- function(SCa, intensity, xx){
+  singleCutVect(SCa, intensity, xx)
 }
-
-#' @rdname gausModel
-#' @export
-gaussC  <- function(c1, Mc, Sc, c2, xx){
-  c1 / (sqrt(2 * pi) * Sc) * exp(-((xx - Mc)^2)/(2 * Sc^2)) +
-    c2 / (sqrt(2 * pi) * Sc * 2) * exp(-((xx - Mc * 2)^2)/(2 * (Sc * 2)^2))
-}
+attr(singleCut, "compName") <- "single cut"
 
 
-#####################
-## Complete Models ##
-#####################
-
-oneSampleSC <-
-  function (xx, a1, Ma, Sa, a2, b1, Mb, Sb, b2, SCa, intensity) { 
-    ## a1 == highest G1 peak
-    a1/(sqrt(2 * pi) * Sa) * exp(-((xx - Ma)^2)/(2 * Sa^2)) +
-      ## a2 == G2 peak for a1
-      a2/(sqrt(2 * pi) * Sa * 2) * exp(-((xx - Ma * 2)^2)/(2 * (Sa * 2)^2)) +
-        ## b1 == second highest peak, assumed to be the other G1 peak
-        b1/(sqrt(2 * pi) * Sb) * exp(-((xx - Mb)^2)/(2 * Sb^2)) +
-        ## b2 == G2 peak for b1
-        b2/(sqrt(2 * pi) * Sb * 2) * exp(-((xx - Mb * 2)^2)/(2 * (Sb * 2)^2)) +
-        ## single cut debris curve
-        singleCut(SCa, intensity, xx) 
-}
-
-oneSampleSCInit <- function(mCall, LHS, data) {
-  ## Not sure we need this fancy stuff, given we have the data already in
-  ## hand, and in order:
+##' Provide starting values for flowHist NLS models
+##'
+##' Given a flowHist object with peaks identified (as should be the case
+##' after calls to \code{flowHist} or \code{pickInit} (and also the
+##' internal functions \code{pickPeaks} or \code{findPeaks},
+##' \code{flowInit} will provide rough guesses for starting parameter
+##' values.
+##'
+##' In most cases, at least with reasonably clean histograms, these guesses
+##' are sufficient for the NLS optimization routine to find appropriate
+##' values.
+##' 
+##' @title flowInit
+##' @param fh a \code{flowHist} object 
+##' @return Returns the \code{flowHist} object with the initial parameter
+##'   estimates in the init slot.
+##' @author Tyler Smith
+##' @export
+flowInit <- function(fh) {
+  xy <- fh$data
+  if(class(fh$peaks) != "matrix")
+    stop("no peaks identified for flowHist object -- see pickPeaks or findPeaks")
   
-  xy <- sortedXyData(mCall[["xx"]], LHS, data)
-  ##xy <- data[, "x", "intensity"]
-  peaks <- findPeaks(xy[, "y"], 20)     # WARNING: window size hardcoded 
-                                        # here!
+  peaks <- fh$peaks
 
-  peaks <- peaks[order(peaks[, "height"], decreasing = TRUE),]
-  Ma <- peaks[1, "mean"]
-  Sa <- Ma / 20                         # assume CV = 0.05
-  a1 <- peaks[1, "height"] * Sa / 0.4
-  if((peaks[1, "mean"] * 2) > max(xy[1, ]))
-    a2 <- 0
-  else
-    a2 <- xy[peaks[1, "mean"] * 2, "intensity"] * Sa * 2 / 0.4
-  Mb <- peaks[2, "mean"]
-  Sb <- Mb / 20
-  b1 <- peaks[2, "height"] * Sb / 0.4
-  if((peaks[2, "mean"] * 2) > max(xy[1, ]))
-    b2 <- 0
-  else
-    b2 <- as.vector(xy[peaks[2, "mean"] * 2, "intensity"] * Sb * 2 / 0.4)
+  params <- names(formals(fh$model))
+  params <- params[-which(params %in% c("", "xx", "intensity"))]
+  value <- c()
+  
+  if("Ma" %in% params) {
+    ## Any model with Ma will require all three of these parameters:
+    Ma <- peaks[1, "mean"]  
+    Sa <- Ma / 20                         # assume CV = 0.05
+    a1 <- peaks[1, "height"] * Sa / 0.4
+    tmpval <- c(Ma, Sa, a1)
+    names(tmpval) <- c("Ma", "Sa", "a1")
+    value <- c(value, tmpval)
+  }
 
-  SCa <- 0.1                            # just a wild guess for now
-  value <- c(a1, Ma, Sa, a2, b1, Mb, Sb, b2, SCa)
-  names(value) <-
-    mCall[c("a1", "Ma", "Sa", "a2", "b1", "Mb", "Sb", "b2", "SCa")] 
-  value
+  if("a2" %in% params) {
+    ## Is a2 off the chart? It shouldn't be! Models with an a2 peak can
+    ## break if the a2 peak is beyond the data range.
+    if((peaks[1, "mean"] * 2) > max(xy[ ,"x"])){
+      warning("a2 peak appears to be out of range")
+      a2 <- 0
+    } else {
+      a2 <- xy[peaks[1, "mean"] * 2, "intensity"] * Sa * 2 / 0.4
+    }
+    tmpval <- c(a2)
+    names(tmpval) <- c("a2")
+    value <- c(value, tmpval)
+  }
+
+  if("Mb" %in% params){
+    Mb <- peaks[2, "mean"]
+    Sb <- Mb / 20
+    b1 <- peaks[2, "height"] * Sb / 0.4
+    tmpval <- c(Mb, Sb, b1)
+    names(tmpval) <- c("Mb", "Sb", "b1")
+    value <- c(value, tmpval)
+  }
+
+  if("b2" %in% params) {
+    if((peaks[2, "mean"] * 2) > max(xy[,"x"])){
+      warning("b2 peak appears to be out of range")
+      b2 <- 0
+    } else {
+      b2 <- as.vector(xy[peaks[2, "mean"] * 2, "intensity"] * Sb * 2 / 0.4)
+    }
+    tmpval <- c(b2)
+    names(tmpval) <- c("b2")
+    value <- c(value, tmpval)
+  }
+
+  if("SCa" %in% params){
+    SCa <- 0.1                            # just a wild guess for now
+    tmpval <- c(SCa)
+    names(tmpval) <- c("SCa")
+    value <- c(value, tmpval)
+  }
+
+  as.list(value)
 }
 
-SSoneSSC <- selfStart(oneSampleSC, oneSampleSCInit,
-                      c("a1", "Ma", "Sa", "a2", "b1", "Mb", "Sb", "b2",
-                        "SCa"))  
+##' Build an NLS model from a list of components
+##'
+##' @title makeModel
+##' @param components a list of model components to combine
+##' @param env the parent frame. Not intended for use by users.
+##' @return a function for use in the R nonlinear regression routine.
+##' @author Tyler Smith
+makeModel <- function(components, env = parent.frame()){
+
+  args <- unlist(lapply(components, formals))
+  args <- args[unique(names(args))]
+  
+  bodList <- lapply(components, FUN = body)
+  bod <- bodList[[1]]
+  bodList <- bodList[-1]
+
+  while(length(bodList) > 0){
+    bod <- call("+", bod, bodList[[1]])
+    bodList <- bodList[-1]
+  }
+
+  eval(call("function", as.pairlist(args), bod), env)
+
+}
+
 
