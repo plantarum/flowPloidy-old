3d10a6e461f71093b60174c82c95e5ac593b59df
Tyler Smith <tyler@plantarum.ca>
2016-06-24 16:41:36 -0400

    updated tests, noted problems that need attention.

--- test-script.R
+++ test-script.R
@@ -1,4 +1,23 @@
 chan = "FL3.INT.LIN"
+files <- list.files(system.file("extdata", package = "flowPloidy"))
+
+i <- 12
+filei <- system.file("extdata", files[i], package = "flowPloidy")
+fhi <- flowHist(FILE = filei, CHANNEL = chan)
+plot(fhi, init = TRUE)
+fhi <- pickInit(fhi)
+fhi <- fhAnalyze(fhi)
+plot(fhi)
+fhi
+
+
+## 734.LMD
+## Something wrong with this one -- no fA2 component in the model, but it
+## should be??
+
+## "337.LMD" requires manually setting the init values
+## "240+2.LMD" requires manually setting the init values
+## "248+r.LMD" check linearity
 
 file1 <- system.file("extdata", "188-15.LMD", package = "flowPloidy")
 fh1 <- flowHist(FILE = file1, CHANNEL = chan)
@@ -13,3 +32,13 @@ plot(fh2, init = TRUE)
 fh2 <- fhAnalyze(fh2)
 plot(fh2)
 fh2
+
+file3 <- system.file("extdata", "226.LMD", package = "flowPloidy")
+fh3 <- flowHist(FILE = file3, CHANNEL = chan)
+plot(fh3, init = TRUE)
+fh3 <- fhAnalyze(fh3)
+plot(fh3)
+fh3
+
+
+222.LMD
