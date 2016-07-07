27d2abcc0659590dd99e2e256a99622abb143fe3
unknown <smithty@ONOTTRA665950D.AGR.GC.CA>
2016-07-07 17:14:18 -0400

    exporting makeModel, in the hopes that that will fix issues building vignettes?

--- R/models.R
+++ R/models.R
@@ -1,10 +1,10 @@
 ## Contains the various model components used in the flowPloidy analysis.
 
 ## Functions for building non-linear models for application to flowHist
-## objects. 
+## objects.
 
 #' Gaussian model components
-#' 
+#'
 #' Components for modeling Gaussian features in flow histograms
 #'
 #' Typically the complete models will contain fA1 and fB2, which model the
@@ -12,7 +12,7 @@
 #' contain fA2 and fB2, which model the G2 peaks. The G2 peaks are linked
 #' to the G1 peaks, in that they require some of the parameters from the
 #' G1 peaks as well (mean and standard deviation).
-#' 
+#'
 #' @param a1,a2,b1,b2 area parameters
 #' @param Ma,Mb curve mean parameter
 #' @param Sa,Sb curve standard deviation parameter
@@ -40,7 +40,7 @@ fB2 <- function(b2, Sb, Mb, xx){
 }
 
 ## Single-cut debris model
-## 
+##
 ## S(x) = a \sum{j = x + 1}^{n} \sqrt[3]{j} Y_j P_s(j, x)
 ## P_s (j, x) = \frac{2}{(\pi j \sqrt{(x/j) (1 - x/j)}
 ##
@@ -62,15 +62,15 @@ fB2 <- function(b2, Sb, Mb, xx){
 #' Single-cut debris model
 #'
 #' Models debris using the single-cut model described by Bagwell et al.
-#' (1991). 
+#' (1991).
 #'
 #' The model is:
 #' \deqn{S(x) = a \sum{j = x + 1}^{n} \sqrt[3]{j} Y_j P_s(j, x)}
-#'  
+#'
 #' x is the histogram channel that we're estimating the debris value for
 #' SCa is the amplitude parameter
 #' Y_j is the histogram intensity for channel j.
-#' 
+#'
 #' where P_s(j, x) is the probability of a nuclei from channel j falling
 #' into channel x when cut. That is, for j > x, the probability that
 #' fragmenting a nuclei from channel j with a single cut will produce a
@@ -85,18 +85,18 @@ fB2 <- function(b2, Sb, Mb, xx){
 #' that we can use with the standard nls toolchain.
 #'
 #' @name singleCut
-#' 
+#'
 #' @param SCa a numeric value, the single-cut amplitude parameter
 #' @param intensity a numeric vector, the histogram intensity in each channel
 #' @param xx an integer vector, the ordered channels corresponding to the
 #'   values in `intensity'.
 #' @return NA
-#' 
+#'
 #' @references Bagwell, C. B., Mayo, S. W., Whetstone, S. D., Hitchcox, S.
 #'   A., Baker, D. R., Herbert, D. J., Weaver, D. L., Jones, M. A. and
 #'   Lovett, E. J. (1991), DNA histogram debris theory and compensation.
-#'   Cytometry, 12: 107-118. doi: 10.1002/cyto.990120203 
-#' 
+#'   Cytometry, 12: 107-118. doi: 10.1002/cyto.990120203
+#'
 #' @author Tyler Smith
 
 #' @rdname singleCut
@@ -122,7 +122,7 @@ singleCutBase <- function(SCa, intensity, xx){
 ## this, the comparisons try to test all values of the xx vector against
 ## first.channel and length(intensity). This raises a warning, and the
 ## results are not what we want. We need them tested one at a time, hence
-## the vectorize here: 
+## the vectorize here:
 singleCutVect <- Vectorize(singleCutBase, "xx")
 
 #' @rdname singleCut
@@ -141,8 +141,8 @@ singleCut <- function(SCa, intensity, xx){
 #' In most cases, at least with reasonably clean histograms, these guesses
 #' are sufficient for the NLS optimization routine to find appropriate
 #' values.
-#' 
-#' @param fh a \code{flowHist} object 
+#'
+#' @param fh a \code{flowHist} object
 #' @return Returns the \code{flowHist} object with the initial parameter
 #'   estimates in the init slot.
 #' @author Tyler Smith
@@ -152,16 +152,16 @@ flowInit <- function(fh) {
   xy <- fh$data
   if(class(fh$peaks) != "matrix")
     stop("no peaks identified for flowHist object -- see pickPeaks or findPeaks")
-  
+
   peaks <- fh$peaks
 
   params <- names(formals(fh$model))
   params <- params[-which(params %in% c("", "xx", "intensity"))]
   value <- c()
-  
+
   if("Ma" %in% params) {
     ## Any model with Ma will require all three of these parameters:
-    Ma <- peaks[1, "mean"]  
+    Ma <- peaks[1, "mean"]
     Sa <- Ma / 20                         # assume CV = 0.05
     a1 <- peaks[1, "height"] * Sa / 0.4
     tmpval <- c(Ma, Sa, a1)
@@ -194,7 +194,7 @@ flowInit <- function(fh) {
       value <- c(value, tmpval)
     }
   }
-  
+
   if("Mb" %in% params){
     Mb <- peaks[2, "mean"]
     Sb <- Mb / 20
@@ -226,7 +226,7 @@ flowInit <- function(fh) {
       names(tmpval) <- c("b2")
       value <- c(value, tmpval)
     }
-  } 
+  }
 
   if("SCa" %in% params){
     SCa <- 0.1                            # just a wild guess for now
@@ -245,17 +245,18 @@ flowInit <- function(fh) {
 #' Build an NLS model from a list of components
 #'
 #' Build an NLS model from a list of components
-#' 
+#'
 #' @param components a list of model components to combine
 #' @param env the parent frame. Not intended for use by users.
 #' @return a function for use in the R nonlinear regression routine.
 #' @author Tyler Smith
+#' @export
 makeModel <- function(components, env = parent.frame()){
 
   names(components) <- NULL
   args <- unlist(lapply(components, formals))
   args <- args[unique(names(args))]
-  
+
   bodList <- lapply(components, FUN = body)
   bod <- bodList[[1]]
   bodList <- bodList[-1]
