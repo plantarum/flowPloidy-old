e29d370c8d85f9b6d10507e7ad39acb6c0878db2
Tyler Smith <tyler@plantarum.ca>
2016-07-08 17:08:07 -0400

    Updated test script, added pander report function

--- test-script.R
+++ test-script.R
@@ -1,10 +1,8 @@
-render("vignettes/flowPloidy-overview.Rmd", output_format = "md_document")
-pandoc flowPloidy-overview.md -o flowPloidy-overview.pdf --listings --template=ty2
-
 library(devtools)
 load_all()
 chan = "FL3.INT.LIN"
-files <- list.files(system.file("extdata", package = "flowPloidy"))
+files <- list.files(system.file("extdata", package = "flowPloidy"),
+                    full.names = TRUE)
 
 i <- 3
 filei <- system.file("extdata", files[i], package = "flowPloidy")
@@ -15,7 +13,41 @@ fhi <- fhAnalyze(fhi)
 plot(fhi)
 fhi
 
+library(pander)
+myReport <- Pandoc$new("Tyler Smith", "flowPloidy Test")
+res <- list()
+for(i in seq_along(files)){
+  message("processing ", files[i])
+  list[[files[i]]] <- NULL
+  filei <- system.file("extdata", files[i], package = "flowPloidy")
+  fhi <- flowHist(FILE = filei, CHANNEL = chan)
+  try(list[[files[[files[i]]] <- fhAnalyze(fhi))
+  myReport$add.paragraph(paste("#", files[i]))
+  myReport$add(exportFlowHist(fhi))
+  myReport$add(plot(fhi))
+}              
 
+
+
+histProcess <- function(author, title, files, chan, outFile, bins = 256, verbose = TRUE){
+  myReport <- Pandoc$new("Tyler Smith", "flowPloidy Test")
+  res <- list()
+
+  for(i in seq_along(files)){
+    if(verbose) message("processing ", files[i])
+    #filei <- system.file("extdata", files[i], package = "flowPloidy")
+    res[[files[i]]] <- flowHist(FILE = files[i], CHANNEL = chan)
+    tryVal <- try(res[[files[i]]] <- fhAnalyze(res[[files[i]]]))
+    if(verbose && inherits(tryVal, "try-error")) message("-- analysis failed")
+    myReport$add.paragraph(paste("#", files[i]))
+    myReport$add(plot(res[[files[i]]], init = TRUE))
+  }              
+
+  myReport$add(exportFlowHist(res, file = outFile))
+  myReport$export()
+  return(res)
+}
+  
 ## 12: 734.LMD requires manually setting the init values
 ## 8: "337.LMD" requires manually setting the init values
 ## "240+S.LMD" requires manually setting the init values
@@ -44,3 +76,4 @@ fh3
 
 
 222.LMD
+
